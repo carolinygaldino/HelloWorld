5a3fc8e1eed1adf564a1ac90cd19adabfba7ac09
Caroliny Galdino <83650298+carolinygaldino@users.noreply.github.com>
2023-04-21 16:40:26 +0100

    Create ControlFlow.cs
    control flow study

---
+++ HelloWorld/ControlFlow.cs
@@ -0,0 +1,26 @@
+using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int ageOfViewer = 12;
+            if(ageOfViewer >= 12)
+            {
+                System.Console.WriteLine("U, PG & 12 films are available.");
+            }
+            
+            else if (ageOfViewer < 15)
+            {
+                System.Console.WriteLine("U, PG, 12 & 15 films are available. ");
+            }
+            
+            ele
+            {
+                System.Console.WriteLine("All films are available");
+            }
+        }
+    }
+}
