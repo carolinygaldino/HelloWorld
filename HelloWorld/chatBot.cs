48d7aeea11ff77becc587132aded2e4a55963c60
Caroliny Galdino <83650298+carolinygaldino@users.noreply.github.com>
2023-04-21 16:59:05 +0100

    Create chatBot.cs

---
+++ HelloWorld/chatBot.cs
@@ -0,0 +1,26 @@
+
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int timeOfDay = 21;
+            if (timeOfDay >= 5 && timeOfDay <= 12)
+            {
+                System.Console.WriteLine("Good morning!");
+            }
+
+            else if (timeOfDay >= 12 && timeOfDay <= 18)
+            {
+                System.Console.WriteLine("Good afternoon!");
+            }
+
+            else
+            {
+                System.Console.WriteLine("Good evening!");
+            }
+        }
+    }
+}
