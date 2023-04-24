f3345cb778865eb208cf2bf93a295e07a60b488f
Caroliny Galdino <83650298+carolinygaldino@users.noreply.github.com>
2023-04-24 16:12:59 +0100

    Create functions.cs

---
+++ HelloWorld/functions.cs
@@ -0,0 +1,14 @@
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void printSomething()
+        {
+            Console.WriteLine("This is a message.");
+        }
+        static void Main(string[] args)
+        {
+            printSomething();
+        }
+    }
+}
