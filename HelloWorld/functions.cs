9feec17ca92cf2ddea9ab2516923989269f4bc56
Caroliny Galdino <83650298+carolinygaldino@users.noreply.github.com>
2023-04-24 16:18:36 +0100

    Update functions.cs

--- HelloWorld/functions.cs
+++ HelloWorld/functions.cs
@@ -2,13 +2,13 @@ namespace MyApp // Note: actual namespace depends on the project name.
 {
     internal class Program
     {
-        static void printSomething(string fullName)
+        static void printSomething(string fullName) //string firstName, string lastName
         {
-            Console.WriteLine("The full name is " + fullName );
+            Console.WriteLine("The full name is " + fullName ); // firstName + " " + lastName
         }
         static void Main(string[] args)
         {
-            printSomething("Chloe gallima");
+            printSomething("Chloe gallima"); //"Chloe", "Gallima"
         }
     }
 }
